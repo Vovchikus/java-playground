d0c357f2d107415ef6fc3f6e99968349fcdfe1dd
vladimir <rampart@yandex.ru>
2016-01-24 18:41:32 +0300

    first commit

---
+++ src/main/java/playground/Playground.java
@@ -0,0 +1,11 @@
+package playground;
+
+
+public class Playground {
+
+    public static void main(String args[]) {
+        Timer timer = new Timer();
+        System.out.println(timer.getLocalCurrentDate());
+    }
+
+}
---
+++ src/main/java/playground/Timer.java
@@ -0,0 +1,12 @@
+package playground;
+
+
+import java.util.Date;
+
+public class Timer {
+
+    public String getLocalCurrentDate() {
+        Date currentDate = new Date();
+        return currentDate.toString();
+    }
+}
